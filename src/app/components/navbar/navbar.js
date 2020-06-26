9a790b8e093b58f3bf592fd68fdb083ffbb98466
Ana Arribasplata <aarribasplata@paytronix.com>
2020-06-26 00:08:30 -0400

    fixed the naviagtion

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -19,12 +19,12 @@ class HomeComponent extends React.Component {
     return (
         <div id="outer-container">
             <Menu onStateChange={ this.isMenuOpen.bind(this) } width={ '50%' } id="scaleRotate" pageWrapId={'page-wrap'} outerContainerId={'outer-container'} >
-                <li className="li-menu-item"><a className="menu-item" href="/about"> About Me </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/hireMe"> Hire Me  </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/travels"> Travels </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/family"> My Family </a></li>
+                <li className="li-menu-item"><a className="menu-item" href="/#/about"> About Me </a></li>
+                <li className="li-menu-item"><a className="menu-item" href="/#/hireMe"> Hire Me  </a></li>
+                <li className="li-menu-item"><a className="menu-item" href="/#/travels"> Travels </a></li>
+                <li className="li-menu-item"><a className="menu-item" href="/#/family"> My Family </a></li>
                 <div className="nav-down"> 
-                    <li className="nav-down-li"><a className="small-icons" href="/hireMe"> Contact Me </a></li>
+                    <li className="nav-down-li"><a className="small-icons" href="/#/hireMe"> Contact Me </a></li>
                     <li className="nav-down-li"><a className="small-icons" href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
                     <li className="nav-down-li"><a className="small-icons" href="/"><FontAwesomeIcon icon={faHome} /></a></li>
                     <li className="nav-down-li"><a className="small-icons" href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
