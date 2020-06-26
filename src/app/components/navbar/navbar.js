fcd23c58b51db7182fddff5ec4581edf03fcadac
Ana Arribasplata <aarribasplata@paytronix.com>
2020-06-26 01:10:21 -0400

    navigation

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -22,20 +22,22 @@ class HomeComponent extends React.Component {
     }
 
     render() {
-        console.log("Render: ", this.state)
+        console.log("Render2: ", this.state)
     return (
         <div id="outer-container">
-            <Menu onStateChange={ this.isMenuOpen.bind(this) } onClick={()=>console.log("On click!")} isOpen={ this.state.isOpen } width={ '50%' } id="scaleRotate" outerContainerId={'outer-container'} >
-                <li className="li-menu-item"><a className="menu-item" href="/#/about"> About Me </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/#/hireMe"> Hire Me  </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/#/travels"> Travels </a></li>
-                <li className="li-menu-item"><a className="menu-item" href="/#/family"> My Family </a></li>
-                <div className="nav-down"> 
-                    <li className="nav-down-li"><a className="small-icons" href="/#/hireMe"> Contact Me </a></li>
-                    <li className="nav-down-li"><a className="small-icons" href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
-                    <li className="nav-down-li"><a className="small-icons" href="/"><FontAwesomeIcon icon={faHome} /></a></li>
-                    <li className="nav-down-li"><a className="small-icons" href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                    <li className="nav-down-li"><a className="small-icons" href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
+            <Menu onStateChange={ this.isMenuOpen.bind(this) } pageWrapId={"page-wrap"} onClick={()=>console.log("On click!")} isOpen={ this.state.isOpen } width={ '50%' } id="scaleRotate" outerContainerId={'outer-container'} >
+                <div id="page-wrap">
+                    <li className="li-menu-item"><a className="menu-item" href="/#/about"> About Me </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/hireMe"> Hire Me  </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/travels"> Travels </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/family"> My Family </a></li>
+                    <div className="nav-down"> 
+                        <li className="nav-down-li"><a className="small-icons" href="/#/hireMe"> Contact Me </a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="/"><FontAwesomeIcon icon={faHome} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
+                    </div>
                 </div>
             </Menu>
         </div>
