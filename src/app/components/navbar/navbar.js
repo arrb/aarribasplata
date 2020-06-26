85777dca7a3fc42a9a4aa2e478772e1e13fe45d6
Ana Arribasplata <aarribasplata@paytronix.com>
2020-06-26 01:16:32 -0400

    onclick function

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -21,21 +21,25 @@ class HomeComponent extends React.Component {
         this.setState({isOpen: !this.state.isOpen})
     }
 
+    closeNav() {
+        this.setState({isOpen: false})
+    }
+
     render() {
-        console.log("Render2: ", this.state)
+        console.log("Re3nder2: ", this.state)
     return (
         <div id="outer-container">
             <Menu onStateChange={ this.isMenuOpen.bind(this) } onClick={()=>console.log("On click!")} isOpen={ this.state.isOpen } width={ '50%' } id="scaleRotate" outerContainerId={'outer-container'} >
-                    <li className="li-menu-item"><a className="menu-item" onClick={ this.isMenuOpen.bind(this) } href="/#/about"> About Me </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ this.isMenuOpen.bind(this) } href="/#/hireMe"> Hire Me  </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ this.isMenuOpen.bind(this) } href="/#/travels"> Travels </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ this.isMenuOpen.bind(this) } href="/#/family"> My Family </a></li>
+                    <li className="li-menu-item"><a className="menu-item" onClick={ this.closeNav.bind(this) } href="/#/about"> About Me </a></li>
+                    <li className="li-menu-item"><a className="menu-item" onClick={ this.closeNav.bind(this) } href="/#/hireMe"> Hire Me  </a></li>
+                    <li className="li-menu-item"><a className="menu-item" onClick={ this.closeNav.bind(this) } href="/#/travels"> Travels </a></li>
+                    <li className="li-menu-item"><a className="menu-item" onClick={ this.closeNav.bind(this) } href="/#/family"> My Family </a></li>
                     <div className="nav-down"> 
-                        <li className="nav-down-li"><a className="small-icons" onClick={ this.isMenuOpen.bind(this) } href="/#/hireMe"> Contact Me </a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ this.isMenuOpen.bind(this) } href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ this.isMenuOpen.bind(this) } href="/"><FontAwesomeIcon icon={faHome} /></a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ this.isMenuOpen.bind(this) } href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ this.isMenuOpen.bind(this) } href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" onClick={ this.closeNav.bind(this) } href="/#/hireMe"> Contact Me </a></li>
+                        <li className="nav-down-li"><a className="small-icons" onClick={ this.closeNav.bind(this) } href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
+                        <li className="nav-down-li"><a className="small-icons" onClick={ this.closeNav.bind(this) } href="/"><FontAwesomeIcon icon={faHome} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" onClick={ this.closeNav.bind(this) } href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" onClick={ this.closeNav.bind(this) } href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
                     </div>
             </Menu>
         </div>
