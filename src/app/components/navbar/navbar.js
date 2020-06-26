6c282789159f34835b79e898b7f341c45731acb1
Ana Arribasplata <aarribasplata@paytronix.com>
2020-06-26 01:28:08 -0400

    Navigation

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -21,25 +21,25 @@ class HomeComponent extends React.Component {
         this.setState({isOpen: !this.state.isOpen})
     }
 
-    closeNav() {
-        this.setState({isOpen: false})
+    test(){
+        console.log("TESTTTTT")
     }
 
     render() {
-        console.log("-- Render: ", this.state)
+        console.log("-k- Render: ", this.state)
     return (
         <div id="outer-container">
-            <Menu onStateChange={ this.isMenuOpen.bind(this) } isOpen={ this.state.isOpen } width={ '50%' } id="scaleRotate" outerContainerId={'outer-container'} >
-                    <li className="li-menu-item"><a className="menu-item" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/#/about"> About Me </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/#/hireMe"> Hire Me  </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/#/travels"> Travels </a></li>
-                    <li className="li-menu-item"><a className="menu-item" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/#/family"> My Family </a></li>
+            <Menu onStateChange={this.test.bind(this)} onStateChange={ this.isMenuOpen.bind(this) } isOpen={ this.state.isOpen } width={ '50%' } id="scaleRotate" outerContainerId={'outer-container'} >
+                    <li className="li-menu-item"><a className="menu-item" href="/#/about"> About Me </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/hireMe"> Hire Me  </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/travels"> Travels </a></li>
+                    <li className="li-menu-item"><a className="menu-item" href="/#/family"> My Family </a></li>
                     <div className="nav-down"> 
-                        <li className="nav-down-li"><a className="small-icons" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/#/hireMe"> Contact Me </a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="/"><FontAwesomeIcon icon={faHome} /></a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                        <li className="nav-down-li"><a className="small-icons" onClick={ ()=>{console.log("Click") , this.setState({isOpen: false}) }} href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="/#/hireMe"> Contact Me </a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="./../../../../arribasplata_ana_resume.pdf" target="_blank"> Resume </a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="/"><FontAwesomeIcon icon={faHome} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="https://www.linkedin.com/in/anaarribasplata" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                        <li className="nav-down-li"><a className="small-icons" href="https://github.com/arrb" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></li>
                     </div>
             </Menu>
         </div>
